85b7fc1dd73d5160aafcc4678031485110faf0c6
Gosia Kurkiewicz <gosiakurkiewicz@gosikurk02m.corp.skyscanner.local>
2020-02-16 12:58:00 +0100

    Refactoring.

--- 6/heightChecker.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def heightChecker(self, heights: List[int]) -> int:
-        comp = sorted(heights)
-        count = 0
-        for i in range(0,len(heights)):
-            if heights[i] != comp[i]:
-                count += 1
-        return count
-
